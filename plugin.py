12ffd4f8f6c50dcd27e2ea6fba90603ab3878768
James Archer <akajea@gmail.com>
2021-01-30 21:17:20 +1100

    Fix .resume/.re commands to use .o

--- plugin.py
+++ plugin.py
@@ -219,12 +219,12 @@ class MoovPlugin(GajimPlugin):
 				self.video_url = session['video_info']['url']
 				self.moov.append(self.video_url)
 				self.moov.seek(session['time'])
-				self.conv.send(f'.{self.video_url} {format_time(session["time"])}')
+				self.conv.send(f'.o {self.video_url} {format_time(session["time"])}')
 				self.send_message(format_status(self.moov.get_status()))
 		elif tokens[0] == '.re':
 			if self.db is not None and alive:
 				time_str = format_time(self.moov.get_status()['time'])
-				self.conv.send(f'.{self.video_url} {time_str}')
+				self.conv.send(f'.o {self.video_url} {time_str}')
 
 
 	def download_info(self, url, callback, conv):
